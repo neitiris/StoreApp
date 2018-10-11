5bb651cd01749c2eb54e0dba02b2336168c2877c
Vlad <veliantos@gmail.com>
2018-10-12 00:20:40 +0300

    created footer created both nav(no responsive yet)

--- src/app/pages/title-page/title-page.component.ts
+++ src/app/pages/title-page/title-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {faSearch, faUserAlt} from '@fortawesome/free-solid-svg-icons';
-import {faUser} from '@fortawesome/free-solid-svg-icons/faUser';
+import {faList, faSearch, faUserAlt} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-title-page',
@@ -9,8 +8,63 @@ import {faUser} from '@fortawesome/free-solid-svg-icons/faUser';
 })
 export class TitlePageComponent implements OnInit {
   faSearchIcon = faSearch;
-  faHumanIcon = faUserAlt
-
+  faHumanIcon = faUserAlt;
+  faMenuIcon = faList;
+  footer_list = [
+    {href: '', text: 'Privacy and Cookies'},
+    {href: '', text: 'Terms and Conditions'},
+    {href: '', text: 'Accessibility'},
+    {href: '', text: 'Store Directory'},
+    {href: '', text: 'About Us'}
+  ];
+  lists = [
+    [
+      {title: 'Costumer Service'},
+      {href: '', text: 'Contact'},
+      {href: '', text: 'FAQ'},
+      {href: '', text: 'Terms of payment'},
+      {href: '', text: 'Terms of sale'},
+      {href: '', text: 'Delivery terms'},
+      {href: '', text: 'Returns and refunds'},
+    ],
+    [
+      {title: 'Costumer Service'},
+      {href: '', text: 'Contact'},
+      {href: '', text: 'FAQ'},
+      {href: '', text: 'Terms of payment'},
+      {href: '', text: 'Terms of sale'},
+      {href: '', text: 'Delivery terms'},
+      {href: '', text: 'Returns and refunds'},
+    ],
+    [
+      {title: 'Costumer Service'},
+      {href: '', text: 'Contact'},
+      {href: '', text: 'FAQ'},
+      {href: '', text: 'Terms of payment'},
+      {href: '', text: 'Terms of sale'},
+      {href: '', text: 'Delivery terms'},
+      {href: '', text: 'Returns and refunds'},
+    ],
+    [
+      {title: 'Costumer Service'},
+      {href: '', text: 'Contact'},
+      {href: '', text: 'FAQ'},
+      {href: '', text: 'Terms of payment'},
+      {href: '', text: 'Terms of sale'},
+      {href: '', text: 'Delivery terms'},
+      {href: '', text: 'Returns and refunds'},
+    ]
+  ];
+  socials = [
+    {
+      href: '', img: '../../../assets/twitter.png',
+      href2: '', img2: '../../../assets/pint.png',
+      href3: '', img3: '../../../assets/facebook.png'},
+    {
+      href: '', img: '../../../assets/g+.png',
+      href2: '', img2: '../../../assets/t.png',
+      href3: '', img3: '../../../assets/v.png'}
+  ];
   constructor() { }
 
   ngOnInit() {
