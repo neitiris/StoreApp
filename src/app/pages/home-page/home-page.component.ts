9c1f474358da862ddb34aeb98e8e2362a019da50
Vlad <veliantos@gmail.com>
2018-10-19 22:20:15 +0300

    created about part created second dress part created brand part

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -25,6 +25,21 @@ export class HomePageComponent implements OnInit {
     '../../../assets/slider-img.png',
     '../../../assets/slider-img.png',
   ];
+  positions = [
+    {img: '../../../assets/girl1.png', name: 'MIMOSA DRESS', cost: '73.74$'},
+    {img: '../../../assets/girl1.png', name: 'MIMOSA DRESS', cost: '73.74$'},
+    {img: '../../../assets/girl1.png', name: 'MIMOSA DRESS', cost: '73.74$'},
+  ];
+  filters = [
+    { sort: 'all', name: 'WHAT`S HOT?', checked: true },
+    { sort: 'recent', name: 'DESIGNERS', checked: false },
+    { sort: 'popular', name: 'FEATURED', checked: false },
+    { sort: 'price', name: 'LATEST', checked: false }
+  ];
+  buttons = [
+    { sort: 'back', name: '<', checked: false },
+    { sort: 'forward', name: '>', checked: true }
+];
   constructor() { }
 
   ngOnInit() {
